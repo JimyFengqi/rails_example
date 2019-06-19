ad1b2fa07453a53ee055f601083853e9b375b165
Jimy_fengqi <jmps515@163.com>
2019-06-19 16:53:09 +0800

    第七章所做内容
    主要添加注册页面内容
    同时添加注册页面的集成测试

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-
   #get  '/home',    to: 'static_pages#home'
-  get  '/help',    to: 'static_pages#help' #, as: '/helf'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get '/signup',   to: 'users#new'
+  get  '/help',       to: 'static_pages#help' #, as: '/helf'
+  get  '/about',      to: 'static_pages#about'
+  get  '/contact',    to: 'static_pages#contact'
+  get  '/signup',     to: 'users#new'
+  post '/signup',     to: 'users#create'
+  get  '/users/show', to: 'users#show'
   #get 'users/new'
 
+  resources :users
+  #resources :users
   #get 'static_pages/home'
   #get 'static_pages/help'
   #get 'static_pages/about'
