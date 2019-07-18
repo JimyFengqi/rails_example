57274aa0e3b6c525f014ce8a9e30003e96e2f87b
Jimy_fengqi <jmps515@163.com>
2019-07-18 14:33:36 +0800

    第十二章所学内容
    与会话和账户激活一样，密码重设虽然没有对应的 Active Record 对象，但也可以看做一个资源；
    Rails 可以生成 Action Mailer 动作和视图，用于发送邮件；
    Action Mailer 支持纯文本邮件和 HTML 邮件；
    与普通的动作和视图一样，在邮件程序的视图中也可以使用邮件程序动作中的实例变量；
    使用生成的令牌创建唯一的 URL，用于重设密码；
    使用哈希摘要安全识别有效的密码重设请求；
    邮件程序的测试和集成测试对确认邮件程序的行为都有用；
    在生产环境可以使用 SendGrid 发送电子邮件。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'login',        to: 'sessions#new'
   post 'login',       to: 'sessions#create'
   delete 'logout',    to: 'sessions#destroy'
@@ -15,6 +17,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only:[:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   #resources :users
   #get 'static_pages/home'
   #get 'static_pages/help'
